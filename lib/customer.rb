9310f7c9d75781b45dfa7605b4d9044c5c60214f
Jennifer Swartz <swartzjenn@gmail.com>
2016-06-25 16:41:05 -0400

    Added customer info; all product and customer info working

---
+++ lib/customer.rb
@@ -0,0 +1,29 @@
+class Customer
+  attr_reader :name
+  
+  @@customers = []
+  
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+  
+  def self.all
+    @@customers
+  end
+  
+  def self.find_by_name(name)
+    @@customers.find { |customer| customer.name == name }
+  end
+
+  
+  private
+  
+  def add_to_customers   
+    if @@customers.map { |customer| customer.name }.include? @name
+      raise DuplicateCustomerError, "#{@name} already exists."
+    else
+      @@customers << self
+    end
+  end
+end
